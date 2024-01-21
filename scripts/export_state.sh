e6075e440cea8d38a68df4059d236885977d6ccc
Donnie Earnest <donnie.earnest@outlook.com>
2024-01-20 20:40:31 -0600

    please work

--- scripts/export_state.sh
+++ scripts/export_state.sh
@@ -5,6 +5,8 @@ STAGE=$2
 PLATFORM=$3
 OUTPUT_FILE=$BUILD_SOURCESDIRECTORY/statevars.sh
 
+echo Exporting state file ${BUILD_SOURCESDIRECTORY}/variables/state.ini for $STAGE on $PLATFORM
+
 awk -v stage="[$STAGE]" -v out="$OUTPUT_FILE" '
 BEGIN {flag=0}
 $0 ~ stage {flag=1; next}
@@ -19,3 +21,10 @@ flag && NF >= 2 {
     }
 }
 ' "$INI_FILE"
+
+echo Completed exporting state file
+
+pwd
+ls -l
+
+cat $OUTPUT_FILE
