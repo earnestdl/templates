e8e8d3367883fc610437a07b61134d0abbc828c1
Donnie Earnest <donnie.earnest@outlook.com>
2024-01-20 20:06:08 -0600

    fix script

--- scripts/export_state.sh
+++ scripts/export_state.sh
@@ -10,15 +10,23 @@ PLATFORM=$3
 # Function to parse INI file and export variables
 parse_ini() {
     awk -F'=' -v stage="[$STAGE]" -v platform="$PLATFORM" '
-    $0 == stage {flag=1; next}
-    /^[/[]/ && flag {flag=0}
-    flag && NF==2 {
-        gsub(/ /, "", $2); # Remove spaces in the value
-        print "export " $1"="$2
+    $0 ~ stage {flag=1; next}
+    /^\[/ && flag {flag=0}
+    flag && NF >= 2 {
+        # Concatenate the field from the 2nd to the last (to include values with '=' in them)
+        value = $2
+        for (i = 3; i <= NF; i++) {
+            value = value "=" $i
+        }
+        # Remove leading and trailing spaces from the key and value
+        gsub(/^ *| *$/, "", $1)
+        gsub(/^ *| *$/, "", value)
+        # Print the export command
+        print "export " $1"=\""value"\""
         if (platform == "ado") {
-            print "echo \"##vso[task.setvariable variable="$1"]"$2"\""
+            print "echo \"##vso[task.setvariable variable="$1"]"value"\""
         } else if (platform == "github") {
-            print $1"="$2" >> $GITHUB_ENV"
+            print $1"=\""value"\" >> $GITHUB_ENV"
         }
     }
     ' "$INI_FILE"
