af47d96b042fe0db02abde02b67ae5172e848ce4
Donnie Earnest <donnie.earnest@outlook.com>
2024-01-20 20:30:45 -0600

    idk

--- scripts/export_state.sh
+++ scripts/export_state.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
 
-# Usage: ./script.sh [ini_file] [stage] [platform]
-# Example: ./script.sh ./variables.ini build_default azdo
-
 INI_FILE=$1
 STAGE=$2
 PLATFORM=$3
+OUTPUT_FILE=statevars.sh
 
-# Function to parse INI file and export variables
-parse_ini() {
-    awk -F'=' -v stage="[$STAGE]" -v platform="$PLATFORM" '
-    $0 ~ stage {flag=1; next}
-    /^\[/ && flag {flag=0}
-    flag && NF >= 2 {
-        # Concatenate the field from the 2nd to the last (to include values with '=' in them)
-        value = $2
-        for (i = 3; i <= NF; i++) {
-            value = value "=" $i
-        }
-        # Remove leading and trailing spaces from the key and value
-        gsub(/^ *| *$/, "", $1)
-        gsub(/^ *| *$/, "", value)
-        # Print the export command
-        print "export " $1"=\""value"\""
-        if (platform == "ado") {
-            print "echo \"##vso[task.setvariable variable="$1"]"value"\""
-        } else if (platform == "github") {
-            print $1"=\""value"\" >> $GITHUB_ENV"
-        }
+awk -v stage="[$STAGE]" -v out="$OUTPUT_FILE" '
+BEGIN {flag=0}
+$0 ~ stage {flag=1; next}
+/^\[/ && flag {flag=0}
+flag && NF >= 2 {
+    if (platform == "ado") {
+        print $1 "=" $2 > out
+        print "export " $1 "=" $2 >> out
+        print "echo \"##vso[task.setvariable variable="$1"]"$2"\"" >> out
+    } else if (platform == "github") {
+        print $1"="$2" >> $GITHUB_ENV"
     }
-    ' "$INI_FILE"
 }
-
-# Export variables
-eval $(parse_ini)
+' "$INI_FILE"
