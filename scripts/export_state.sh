b92060084645aaab0c08b697d65db38d5cf655a3
Donnie Earnest <donnie.earnest@outlook.com>
2024-01-21 05:55:13 -0600

    add script

--- scripts/export_state.sh
+++ scripts/export_state.sh
@@ -2,36 +2,30 @@
 
 INI_FILE=$1
 STAGE=$2
-OUTPUT_FILE=statevars.sh
+OUTPUT_FILE="variables.sh"
 
 echo Current state:
 echo ------------------------------------------------------------------------------------
-awk -v stage="$STAGE" -v RS='' '/\[build_'stage'\]/' "$BUILD_SOURCESDIRECTORY/variables/state.ini"
+awk -v RS= -v stage="[${STAGE}]" '$0 ~ stage' "$INI_FILE"
 echo ------------------------------------------------------------------------------------
 
-# TODO THIS
-# Read each line from the input file
+# Extract the relevant section using awk and format it
+awk -v RS= -v stage="[${STAGE}]" '$0 ~ stage' "$INI_FILE" > "$OUTPUT_FILE"
+sed -i -e '1d' -e 's/ \?= \?/=/g' "$OUTPUT_FILE"
+
+# Read and export variables
 while IFS='=' read -r key value; do
-    # Trim leading and trailing whitespace
-    key=$(echo "$key" | xargs)
-    value=$(echo "$value" | xargs)
-    
-    # Write the formatted line to the output file
-    echo "export $key=$value && echo ##vso[task.setvariable variable=$key]$value" >> "$OUTPUT_FILE"
-done < "$INPUT_FILE"
+    if [[ -n $key && -n $value ]]; then
+        # Trim leading and trailing whitespace
+        key=$(echo "$key" | xargs)
+        value=$(echo "$value" | xargs)
+        
+        # Export variable to current shell and to AzDO environment
+        export "$key=$value"
+        echo "##vso[task.setvariable variable=$key]$value"
+    fi
+done < "$OUTPUT_FILE"
 
-# Make the output file executable
-chmod +x "$OUTPUT_FILE"
-
-
-
-echo Exporting state variables for $STAGE on $PLATFORM
-awk -v stage="$STAGE" -v RS='' '/\[build_'stage'\]/' "$BUILD_SOURCESDIRECTORY/variables/state.ini" > $OUTPUT_FILE
-cat $OUTPUT_FILE
-sed -i -e "1d" $OUTPUT_FILE
-source $OUTPUT_FILE
-echo Completed exporting state file
-
-echo System Environment Variables:
+echo System environment variables:
 echo ------------------------------------------------------------------------------------
 env | sort
