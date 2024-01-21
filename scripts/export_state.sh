ae04352a17acf44dd7e353e2e68e56464c90d09f
Donnie Earnest <donnie.earnest@outlook.com>
2024-01-20 20:51:11 -0600

    cleaner

--- scripts/export_state.sh
+++ scripts/export_state.sh
@@ -1,30 +1,19 @@
 #!/bin/bash
-
 INI_FILE=$1
 STAGE=$2
-PLATFORM=$3
 OUTPUT_FILE=$BUILD_SOURCESDIRECTORY/statevars.sh
 
-echo Exporting state file ${BUILD_SOURCESDIRECTORY}/variables/state.ini for $STAGE on $PLATFORM
+echo Current state:
+echo ------------------------------------------------------------------------------------
+awk -v RS= '/\[build_$STAGE\]/' $BUILD_SOURCESDIRECTORY/variables/state.ini > $OUTPUT_FILE
+cat $OUTPUT_FILE
+sed -i -e "1d" $OUTPUT_FILE
+echo ------------------------------------------------------------------------------------
 
-awk -v stage="[$STAGE]" -v out="$OUTPUT_FILE" '
-BEGIN {flag=0}
-$0 ~ stage {flag=1; next}
-/^\[/ && flag {flag=0}
-flag && NF >= 2 {
-    if (platform == "ado") {
-        print $1 "=" $2 > out
-        print "export " $1 "=" $2 >> out
-        print "echo \"##vso[task.setvariable variable="$1"]"$2"\"" >> out
-    } else if (platform == "github") {
-        print $1"="$2" >> $GITHUB_ENV"
-    }
-}
-' "$INI_FILE"
-
+echo Exporting state variables for $STAGE on $PLATFORM
+source $OUTPUT_FILE
 echo Completed exporting state file
 
-pwd
-ls -l
-
-cat $OUTPUT_FILE
+echo System Environment Variables:
+echo ------------------------------------------------------------------------------------
+env | sort
