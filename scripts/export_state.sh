1cf10b80115d2e1648d0f06eae5d1605517809cf
Donnie Earnest <donnie.earnest@outlook.com>
2024-01-20 20:56:56 -0600

    wtf

--- scripts/export_state.sh
+++ scripts/export_state.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 INI_FILE=$1
 STAGE=$2
-OUTPUT_FILE=$BUILD_SOURCESDIRECTORY/statevars.sh
+OUTPUT_FILE=statevars.sh
 
 echo Current state:
 echo ------------------------------------------------------------------------------------
-awk -v RS= '/\[build_$STAGE\]/' $BUILD_SOURCESDIRECTORY/variables/state.ini > $OUTPUT_FILE
-cat $OUTPUT_FILE
-sed -i -e "1d" $OUTPUT_FILE
+awk -v RS= '/\[build_$STAGE\]/' $BUILD_SOURCESDIRECTORY/variables/state.ini
 echo ------------------------------------------------------------------------------------
 
 echo Exporting state variables for $STAGE on $PLATFORM
+awk -v RS= '/\[build_$STAGE\]/' $BUILD_SOURCESDIRECTORY/variables/state.ini > $OUTPUT_FILE
+cat $OUTPUT_FILE
+sed -i -e "1d" $OUTPUT_FILE
 source $OUTPUT_FILE
 echo Completed exporting state file
 
