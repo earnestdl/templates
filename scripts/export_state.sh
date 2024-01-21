40cb3b392dc29289f972eab0b3ec367bde9dddcf
Donnie Earnest <donnie.earnest@outlook.com>
2024-01-21 06:11:20 -0600

    update script

--- scripts/export_state.sh
+++ scripts/export_state.sh
@@ -4,21 +4,21 @@ INI_FILE=$1
 STAGE=$2
 OUTPUT_FILE="variables.sh"
 
-echo Processing state for $STAGE:
-echo ------------------------------------------------------------------------------------
-awk -v RS='/\[$STAGE\]/' "$INI_FILE" > "$OUTPUT_FILE"
-sed -i -e '1d' -e 's/ \?= \?/=/g' "$OUTPUT_FILE"
+echo "Processing state for $STAGE:"
+echo "------------------------------------------------------------------------------------"
+awk -v stage="\\[$STAGE\\]" '$0 ~ stage,/^\\[/' "$INI_FILE" | sed -e '1d' -e 's/ \?= \?/=/g' > "$OUTPUT_FILE"
 cat "$OUTPUT_FILE"
-echo ------------------------------------------------------------------------------------
+echo "------------------------------------------------------------------------------------"
 
-echo Exporting variables:
-echo ------------------------------------------------------------------------------------
+echo "Exporting variables:"
+echo "------------------------------------------------------------------------------------"
+
 while IFS='=' read -r key value; do
     if [[ -n $key && -n $value ]]; then
         # Trim leading and trailing whitespace
         key=$(echo "$key" | xargs)
         value=$(echo "$value" | xargs)
-        echo "$key=$value"        
+        echo "$key=$value"
         export "$key=$value"
         echo "##vso[task.setvariable variable=$key]$value"
     fi
