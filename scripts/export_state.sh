d03303cc9e5c5c8549d78abae5c626778c88d2a8
Donnie Earnest <donnie.earnest@outlook.com>
2024-01-20 21:07:24 -0600

    fixed awk

--- scripts/export_state.sh
+++ scripts/export_state.sh
@@ -5,11 +5,11 @@ OUTPUT_FILE=statevars.sh
 
 echo Current state:
 echo ------------------------------------------------------------------------------------
-awk -v RS= '/\[build_$STAGE\]/' $BUILD_SOURCESDIRECTORY/variables/state.ini
+awk -v stage="$STAGE" -v RS='' '/\[build_'stage'\]/' "$BUILD_SOURCESDIRECTORY/variables/state.ini"
 echo ------------------------------------------------------------------------------------
 
 echo Exporting state variables for $STAGE on $PLATFORM
-awk -v RS= '/\[build_$STAGE\]/' $BUILD_SOURCESDIRECTORY/variables/state.ini > $OUTPUT_FILE
+awk -v stage="$STAGE" -v RS='' '/\[build_'stage'\]/' "$BUILD_SOURCESDIRECTORY/variables/state.ini" > $OUTPUT_FILE
 cat $OUTPUT_FILE
 sed -i -e "1d" $OUTPUT_FILE
 source $OUTPUT_FILE
