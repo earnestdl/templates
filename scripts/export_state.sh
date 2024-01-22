ea1aee98d102181049fb82c57d4123678ad26e5e
Donnie Earnest <donnie.earnest@outlook.com>
2024-01-21 18:34:50 -0600

    github or azdo

--- scripts/export_state.sh
+++ scripts/export_state.sh
@@ -4,7 +4,16 @@ INI_FILE=$1
 STAGE=$2
 OUTPUT_FILE="variables.sh"
 
-echo "Processing state for $STAGE:"
+# Detecting platform
+if [ -n "$GITHUB_ACTIONS" ]; then
+    PLATFORM='github'
+elif [ -n "$AZURE_HTTP_USER_AGENT" ]; then
+    PLATFORM='azdo'
+else
+    PLATFORM='shell'
+fi
+
+echo "Processing state for $STAGE on $PLATFORM:"
 echo "---------------------------------------------------"
 awk -v RS='' "/\\[$STAGE\\]/" "$INI_FILE" > "$OUTPUT_FILE"
 while IFS='=' read -r key value; do
@@ -12,7 +21,21 @@ while IFS='=' read -r key value; do
         key=$(echo "$key" | xargs)
         value=$(echo "$value" | xargs)
         echo "$key=$value"
-        export "$key=$value"
-        echo "##vso[task.setvariable variable=$key]$value"
+        
+        case $PLATFORM in
+        'github')
+            # Handling for GitHub Actions
+            echo "::set-env name=$key::$value"
+            ;;
+        'azdo')
+            # Handling for Azure DevOps
+            export "$key=$value"
+            echo "##vso[task.setvariable variable=$key]$value"
+            ;;
+        'shell')
+            # Default shell export
+            export "$key=$value"
+            ;;
+        esac
     fi
 done < "$OUTPUT_FILE"
