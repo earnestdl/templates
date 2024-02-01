543c2c1323c756c572d01e9eb04027a078058803
Donnie Earnest <donnie.earnest@outlook.com>
2024-01-31 19:25:02 -0600

    refactor

--- scripts/defaults/default.sh
+++ scripts/defaults/default.sh
@@ -2,101 +2,118 @@
 
 # Function for pre-build steps
 pre-build() {
-    echo ----------------------------
+    echo
+    echo -----------------------------------------------------------
     echo "Running pre-build steps..."
     echo  
     echo  ...
     echo 
     echo "Pre-build steps complete."
-    echo ----------------------------
+    echo -----------------------------------------------------------
+    echo
 }
 
 # Function for build steps
 build() {
-    echo ----------------------------
+    echo
+    echo -----------------------------------------------------------
     echo "Building..."
     echo  
     echo  ...
     echo 
     echo "Build complete."
-    echo ----------------------------
+    echo -----------------------------------------------------------
+    echo
 }
 
 # Function for post-build steps
 post-build() {
-    echo ----------------------------
+    echo
+    echo -----------------------------------------------------------
     echo "Running post-build steps..."
     echo  
     echo  ...
     echo 
     echo "Post-build complete."
-    echo ----------------------------
+    echo -----------------------------------------------------------
+    echo
 }
 
 # Function for pre-test steps
 pre-test() {
-    echo ----------------------------
+    echo
+    echo -----------------------------------------------------------
     echo "Running pre-test steps..."
     echo  
     echo  ...
     echo 
     echo "Pre-test steps complete."
-    echo ----------------------------
+    echo -----------------------------------------------------------
+    echo
 }
 
 # Function for test steps
 test() {
-    echo ----------------------------
+    echo
+    echo -----------------------------------------------------------
     echo "Testing..."
     echo  
     echo  ...
     echo 
     echo "Testing complete."
-    echo ----------------------------
+    echo -----------------------------------------------------------
+    echo
 }
 
 # Function for post-test steps
 post-test() {
-    echo ----------------------------
+    echo
+    echo -----------------------------------------------------------
     echo "Running post-test steps..."
     echo  
     echo  ...
     echo 
     echo "Pre-test steps complete."
-    echo ----------------------------
+    echo -----------------------------------------------------------
+    echo
 }
 
 # Function for pre-deploy steps
 pre-deploy() {
-    echo ----------------------------
+    echo
+    echo -----------------------------------------------------------
     echo "Running pre-deploy steps..."
     echo  
     echo  ...
     echo 
     echo "Pre-deploy steps complete."
-    echo ----------------------------
+    echo -----------------------------------------------------------
+    echo
 }
 
 # Function for deploy steps
 deploy() {
-    echo ----------------------------
+    echo
+    echo -----------------------------------------------------------
     echo "Deploying..."
     echo  
     echo  ...
     echo 
     echo "Deployment complete."
-    echo ----------------------------
+    echo -----------------------------------------------------------
+    echo
 }
 
 # Function for post-deploy steps
 post-deploy() {
-    echo ----------------------------
+    echo
+    echo -----------------------------------------------------------
     echo "Running post-build steps..."
     echo  
     echo  ...
     echo 
     echo "Post-build steps complete."
-    echo ----------------------------
+    echo -----------------------------------------------------------
     echo
     echo ---------------------------------------------------------------------------------------------------------------------
     echo "To use your own script, you can place it in a 'scripts' subfolder next to your pipeline.yml or workflow.yml."
@@ -107,18 +124,18 @@ post-deploy() {
     echo "Don't forget to also change your PRE_SCRIPT_ARGS, POST_SCRIPT_ARGS, APP_BUILD_ARGS and APP_DEPLOY_ARGS accordingly." 
     echo
     echo
-    echo
     echo "List of process scripts available:"
+    echo -----------------------------------------------------------
     ls -l ${CDP_PROCESS_SCRIPTS}
-    echo 
     echo
     echo
     echo "List of steps scripts available:"
+    echo -----------------------------------------------------------
     ls -l ${CDP_STEPS_SCRIPTS}
-    echo 
     echo
     echo
     echo "List of state scripts available: (scripts from your repo)"
+    echo -----------------------------------------------------------
     ls -l ${CDP_STATE_SCRIPTS}
     echo 
 }
